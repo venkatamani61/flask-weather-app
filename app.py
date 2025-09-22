b39510579288fc8d4963e2e7545efc7d46270b81
Venkata Mani Vallepu <venkatamanivallepu@gmail.com>
2025-09-22 10:50:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import requests
 import os
 app = Flask(__name__)
-API_KEY = "be76618aaf010e326987821d5b39e939"
+API_KEY = os.environ.get("OPENWEATHER_API_KEY")  # safer way
 
 def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
